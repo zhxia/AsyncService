20e784635da369fd0c0cfcd1a90557f2897dab3d
zhxia <xiazhenghong@chelun.com>
2016-08-05 10:04:22 +0800

    modify

---
+++ configs/product/common.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhxia84@gmail.com
+ * Date: 8/3/16
+ * Time: 2:08 PM
+ */
+
+return array(
+    'redis' => array('ip' => '192.168.1.234', 'port' => 7000),
+);
